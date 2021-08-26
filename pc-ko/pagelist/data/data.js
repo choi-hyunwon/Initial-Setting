b0145d2a64854adf219bf222c1adc1febebc8d4b
bcde77 <domfam92@gmail.com>
2021-08-26 19:47:23 +0900

    뉴스룸 페이지 작업 예정

--- pc-ko/pagelist/data/data.js
+++ pc-ko/pagelist/data/data.js
@@ -4,19 +4,43 @@ data.newsroom = {
   list: [
     {
       stateClass: 'complete',
-      url: 'board_01',
+      url: 'newsroom_00',
       category: 'newsroom',
       page: '썸네일 리스트'
     },
     {
       stateClass: 'complete',
-      url: 'board_02',
+      url: 'newsroom_01',
       category: 'newsroom',
       page: '게시판 리스트 1'
     },
     {
       stateClass: 'complete',
-      url: 'board_03',
+      url: 'newsroom_02',
+      category: 'newsroom',
+      page: '게시판 리스트 2'
+    },
+    {
+      stateClass: 'complete',
+      url: 'newsroom_03',
+      category: 'newsroom',
+      page: '게시판 리스트 2'
+    },
+    {
+      stateClass: 'complete',
+      url: 'newsroom_04',
+      category: 'newsroom',
+      page: '게시판 리스트 2'
+    },
+    {
+      stateClass: 'complete',
+      url: 'newsroom_05',
+      category: 'newsroom',
+      page: '게시판 리스트 2'
+    },
+    {
+      stateClass: 'complete',
+      url: 'newsroom_06',
       category: 'newsroom',
       page: '게시판 리스트 2'
     }
