d6eb3d348c7b53414e1a774eba8b96b9a2ac8075
Juyeon-0919 <60646121+Juyeon-0919@users.noreply.github.com>
2021-09-09 10:30:32 +0900

    지속가능경영 > 사회공헌 > 윤리경영 완료

--- pc-ko/pagelist/data/data.js
+++ pc-ko/pagelist/data/data.js
@@ -145,6 +145,12 @@ data.macrogen = {
       page: '01Macrogen_03지속가능경영_01윤리경영'
     },
     {
+      stateClass: 'waiting',
+      url: '01Macrogen_03지속가능경영_02사회공헌_01소개',
+      category: 'macrogen',
+      page: '01Macrogen_03지속가능경영_02사회공헌_01소개'
+    },
+    {
       stateClass: 'complete',
       url: '01Macrogen_04채용_03채용안내_01목록',
       category: 'macrogen',
