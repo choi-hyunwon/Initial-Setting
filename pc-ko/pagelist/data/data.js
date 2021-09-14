dd81c44a6cea3974941f738195d5999ea838ce50
Juyeon-0919 <60646121+Juyeon-0919@users.noreply.github.com>
2021-09-14 13:38:09 +0900

    * 채용안내 > 목록 : tooltip 위치 수정

--- pc-ko/pagelist/data/data.js
+++ pc-ko/pagelist/data/data.js
@@ -200,6 +200,12 @@ data.macrogen = {
     },
     {
       stateClass: 'complete',
+      url: '01Macrogen_03지속가능경영_02사회공헌_03과학자상상세',
+      category: 'macrogen',
+      page: '01Macrogen_03지속가능경영_02사회공헌_03과학자상상세'
+    },
+    {
+      stateClass: 'complete',
       url: '01Macrogen_04채용_01인재상',
       category: 'macrogen',
       page: '01Macrogen_04채용_01인재상'
