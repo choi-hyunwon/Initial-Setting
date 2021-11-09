1af7dc3f03bf61d9464368932c8f8081af2561f1
taeyoon <Gotaeyoon007@gmail.com>
2021-11-09 13:12:40 +0900

    다국어 버전 작업세팅

---
+++ pc-en/gulpfile.js
@@ -0,0 +1,173 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const concat = require('gulp-concat');
+const html = require('gulp-file-include');
+const del = require('del');
+const htmlbeautify = require('gulp-html-beautify');
+const replace = require('gulp-replace');
+const sass = require('gulp-sass');
+sass.compiler = require('node-sass');
+const sourcemaps = require('gulp-sourcemaps');
+
+
+/* scss TASK*/
+function scss() {
+  return gulp.src('src/scss/*.scss')
+      .pipe(sourcemaps.init())
+      .pipe(sass().on('error', sass.logError))
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest('dist/css/'))
+}
+
+function htmlPage() {
+  return gulp.src('src/html/**/*.html')
+      .pipe(html())
+      .pipe(gulp.dest('dist/html/'))
+}
+
+function copyCss() {
+  gulp.src(['src/scss/**/*.scss'])
+      .pipe(gulp.dest('dist/css'));
+  return gulp.src('src/scss/fonts/**',)
+      .pipe(gulp.dest('dist/css/fonts'));
+}
+function copyIndex() {
+  return gulp.src('src/index.html')
+    .pipe(gulp.dest('dist/'));
+}
+function copyImg() {
+  return gulp.src('src/img/**/**')
+    .pipe(gulp.dest('dist/img'));
+}
+function copyFavicon() {
+  return gulp.src('src/favicon/**')
+      .pipe(gulp.dest('dist/favicon'));
+}
+
+function copyFonts() {
+  return gulp.src('src/fonts/**/**')
+    .pipe(gulp.dest('dist/fonts'));
+}
+function copyJS() {
+  return gulp.src('src/js/**')
+      .pipe(gulp.dest('dist/js'));
+}
+function copyCSS() {
+  return gulp.src('src/css/**')
+    .pipe(gulp.dest('dist/css'));
+}
+
+function jsLib() {
+  let sourceLib = [
+    'src/js/lib/jquery.js',
+    'src/js/lib/bootstrap.bundle.min.js',
+    'src/js/lib/swiper.min.js',
+    'src/js/lib/jquery.sticky.js',
+    'src/js/lib/jquery.grid-a-licious.js',
+    'src/js/lib/aos.js',
+    'src/js/lib/jquery-ui.min.js',
+  ];
+  return gulp.src(sourceLib)
+    .pipe(concat('bundle.js'))
+    .pipe(gulp.dest('dist/js'))
+}
+
+function cssIE() {
+  let sourceLib = [
+    'src/scss/lib/bootstrap-ie11.css',
+  ];
+  return gulp.src(sourceLib)
+      .pipe(concat('bootstrap-ie11.css'))
+      .pipe(gulp.dest('dist/css'))
+}
+
+function cssMapIE() {
+  let sourceLib = [
+    'src/scss/lib/bootstrap-ie11.css.map',
+  ];
+  return gulp.src(sourceLib)
+      .pipe(concat('bootstrap-ie11.css.map'))
+      .pipe(gulp.dest('dist/css'))
+}
+
+function jsCommon() {
+  let sourceUi = ['src/js/common.js'];
+  return gulp.src(sourceUi)
+    .pipe(concat('common.js'))
+    .pipe(gulp.dest('dist/js'))
+}
+
+function watchScss() {
+  gulp.watch(['src/scss/**/*.scss','src/scss/*.html'], gulp.series(scss));
+}
+
+function watchHtml() {
+  gulp.watch(['src/html/**/*.html','src/**.html'], gulp.series(htmlPage));
+}
+
+function watchInclude() {
+  gulp.watch('src/html/include/*.html', gulp.series(htmlPage));
+}
+
+function watchJs() {
+  gulp.watch('src/js/*/*.js', gulp.series(jsLib, jsCommon));
+}
+
+function watchImg() {
+  gulp.watch('src/img/**/*', gulp.series(copyImg));
+}
+
+function watchFont() {
+  gulp.watch('src/fonts/**/**', gulp.series(copyFonts));
+}
+
+
+function beautify() {
+  var options = {
+    indentSize: 4
+  }
+  return gulp.src('./dist/html/*.html')
+    .pipe(htmlbeautify(options))
+    .pipe(gulp.dest('./dist/html/'))
+}
+
+function delDist() {
+  return del('dist');
+}
+function delInclude() {
+  return del('dist/html/include');
+}
+
+function setEnvProduct(cb) {
+  process.env.NODE_ENV = 'product';
+  cb();
+}
+
+function setEnvDevelope(cb) {
+  process.env.NODE_ENV = 'develope'
+  cb();
+}
+
+
+//task
+gulp.task("dev", gulp.series(setEnvDevelope, delDist, cssIE, cssMapIE, scss, copyIndex, copyImg, copyFavicon, copyFonts, copyCSS,copyJS, htmlPage,delInclude));
+gulp.task("dist", gulp.series(setEnvProduct, delDist, copyCss, copyImg, copyFonts, copyCSS,copyJS,  htmlPage, delInclude,beautify));
+gulp.task("watch", gulp.parallel(watchScss, watchHtml, watchInclude, watchImg ));
+
+gulp.task('browser-sync', function() {
+  browserSync.init({
+    port: 1001,
+    server: {
+      baseDir: "./",
+      index: "dist/index.html"
+    }
+  });
+  gulp.watch('src/scss/**/*.scss', gulp.series(scss)).on('change', browserSync.reload);
+  gulp.watch(['src/html/**/*.html'], gulp.series(htmlPage)).on('change', browserSync.reload);
+  gulp.watch('src/html/include/*.html', gulp.series(htmlPage)).on('change', browserSync.reload);
+  gulp.watch('src/js/*/*.js', gulp.series(jsLib, jsCommon)).on('change', browserSync.reload);
+  gulp.watch('src/img/**/*', gulp.series(copyImg)).on('change', browserSync.reload);
+  gulp.watch('src/fonts/**/**', gulp.series(copyFonts)).on('change', browserSync.reload);
+});
+
+exports.default = gulp.series("dist");
---
+++ pc-en/pagelist/data/data.js
@@ -0,0 +1,504 @@
+const data = {}
+data.guide = {
+  title: 'Guide',
+  list: [
+    {
+      stateClass: 'complete',
+      url: 'color',
+      category: 'guide',
+      page: 'COLORS'
+    },
+    {
+      stateClass: 'complete',
+      url: 'icon',
+      category: 'guide',
+      page: 'icon'
+    },
+    {
+      stateClass: 'complete',
+      url: 'table',
+      category: 'guide',
+      page: 'table'
+    },
+    {
+      stateClass: 'complete',
+      url: 'button',
+      category: 'guide',
+      page: 'button'
+    },
+    {
+      stateClass: 'complete',
+      url: 'typography_01',
+      category: 'guide',
+      page: 'TYPOGRAPHY'
+    },
+    {
+      stateClass: 'complete',
+      url: 'typography_02',
+      category: 'guide',
+      page: 'TYPOGRAPHY(한글)'
+    },
+    {
+      stateClass: 'complete',
+      url: 'typography_03',
+      category: 'guide',
+      page: 'TYPOGRAPHY(영어)'
+    },
+    {
+      stateClass: 'complete',
+      url: 'grid',
+      category: 'guide',
+      page: 'grid'
+    },
+    {
+      stateClass: 'complete',
+      url: 'layout_01',
+      category: 'guide',
+      page: 'layout frame A'
+    },
+    {
+      stateClass: 'complete',
+      url: 'layout_02',
+      category: 'guide',
+      page: 'layout frame B'
+    },
+    {
+      stateClass: 'complete',
+      url: 'layout_03',
+      category: 'guide',
+      page: 'layout frame C'
+    },
+    {
+      stateClass: 'complete',
+      url: 'input',
+      category: 'guide',
+      page: 'INPUT'
+    },
+    {
+      stateClass: 'complete',
+      url: 'tab',
+      category: 'guide',
+      page: 'tab'
+    },
+    {
+      stateClass: 'complete',
+      url: 'slider',
+      category: 'guide',
+      page: 'slider'
+    },
+    {
+      stateClass: 'complete',
+      url: 'board_01',
+      category: 'guide',
+      page: '썸네일 리스트'
+    },
+    {
+      stateClass: 'complete',
+      url: 'board_02',
+      category: 'guide',
+      page: '게시판 리스트'
+    },
+    {
+      stateClass: 'complete',
+      url: 'board_04',
+      category: 'guide',
+      page: '상세'
+    },
+    {
+      stateClass: 'complete',
+      url: 'Indicator',
+      category: 'guide',
+      page: '인디케이터'
+    },
+    {
+      stateClass: 'complete',
+      url: 'tooltip',
+      category: 'guide',
+      page: 'tooltip'
+    },
+    {
+      stateClass: 'complete',
+      url: 'modal',
+      category: 'guide',
+      page: 'modal'
+    },
+    {
+      stateClass: 'complete',
+      url: 'loading',
+      category: 'guide',
+      page: 'loading'
+    }
+  ]
+}
+
+data.macrogen = {
+  title: 'Macrogen',
+  list: [
+    {
+      stateClass: 'complete',
+      url: 'Main',
+      category: 'macrogen',
+      page: 'Main'
+    },
+    {
+      stateClass: 'complete',
+      url: '01Macrogen_01기업개요',
+      category: 'macrogen',
+      page: '01Macrogen_01기업개요'
+    },
+    {
+      stateClass: 'complete',
+      url: '01Macrogen_02경영진',
+      category: 'macrogen',
+      page: '01Macrogen_02경영진'
+    },
+    {
+      stateClass: 'complete',
+      url: '01Macrogen_03연혁',
+      category: 'macrogen',
+      page: '01Macrogen_03연혁'
+    },
+    {
+      stateClass: 'complete',
+      url: '01Macrogen_04비전',
+      category: 'macrogen',
+      page: '01Macrogen_04비전'
+    },
+    {
+      stateClass: 'complete',
+      url: '01Macrogen_05인증',
+      category: 'macrogen',
+      page: '01Macrogen_05인증'
+    },
+    {
+      stateClass: 'waiting',
+      url: '01Macrogen_06ContactUs',
+      category: 'macrogen',
+      page: '01Macrogen_06ContactUs'
+    },
+    {
+      stateClass: 'complete',
+      url: '01Macrogen_06글로벌네트워크',
+      category: 'macrogen',
+      page: '01Macrogen_06글로벌네트워크'
+    },
+    {
+      stateClass: 'complete',
+      url: '01Macrogen_04채용_01인재상',
+      category: 'macrogen',
+      page: '01Macrogen_04채용_01인재상'
+    },
+    {
+      stateClass: 'complete',
+      url: '01Macrogen_04채용_02인사제도',
+      category: 'macrogen',
+      page: '01Macrogen_04채용_02인사제도'
+    },
+    {
+      stateClass: 'complete',
+      url: '01Macrogen_04채용_03채용안내_01목록',
+      category: 'macrogen',
+      page: '01Macrogen_04채용_03채용안내_01목록'
+    },
+    {
+      stateClass: 'complete',
+      url: '01Macrogen_04채용_03채용안내_02조회',
+      category: 'macrogen',
+      page: '01Macrogen_04채용_03채용안내_02조회'
+    },
+  ]
+}
+data.business ={
+  title: 'BUSINESS',
+  list: [
+    {
+      stateClass: 'complete',
+      url: '연구서비스_NGS',
+      category: 'business',
+      page: '연구서비스_NGS'
+    },
+    {
+      stateClass: 'complete',
+      url: '연구서비스_CES',
+      category: 'business',
+      page: '연구서비스_CES'
+    },
+    {
+      stateClass: 'complete',
+      url: '연구서비스_Oligo',
+      category: 'business',
+      page: '연구서비스_Oligo'
+    },
+    {
+      stateClass: 'complete',
+      url: '연구서비스_MICROARRAY',
+      category: 'business',
+      page: '연구서비스_MICROARRAY'
+    },
+    {
+      stateClass: 'complete',
+      url: '연구서비스_동물모델사업부',
+      category: 'business',
+      page: '연구서비스_동물모델사업부'
+    },
+    {
+      stateClass: 'complete',
+      url: '임상진단서비스_암유전체검사',
+      category: 'business',
+      page: '임상진단서비스_암유전체검사'
+    },
+    {
+      stateClass: 'complete',
+      url: '임상진단서비스_신생아유전자검사',
+      category: 'business',
+      page: '임상진단서비스_신생아유전자검사'
+    },
+    {
+      stateClass: 'complete',
+      url: '임상진단서비스_covid19',
+      category: 'business',
+      page: '임상진단서비스_covid19'
+    },
+    {
+      stateClass: 'complete',
+      url: '헬스케어서비스_개인유전자분석',
+      category: 'business',
+      page: '헬스케어서비스_개인유전자분석'
+    },
+    {
+      stateClass: 'complete',
+      url: '헬스케어서비스_장내미생물분석',
+      category: 'business',
+      page: '헬스케어서비스_장내미생물분석'
+    },
+    {
+      stateClass: 'complete',
+      url: '헬스케어서비스_반려동물유전자분석',
+      category: 'business',
+      page: '헬스케어서비스_반려동물유전자분석'
+    },
+  ]
+}
+data.RD = {
+  title: 'R&D',
+  list: [
+    {
+      stateClass: 'complete',
+      url: '03R&D_01정밀의학연구소_01소개',
+      category: 'RD',
+      page: '03R&D_01정밀의학연구소_01소개'
+    },
+    {
+      stateClass: 'complete',
+      url: '03R&D_02생명정보학연구소',
+      category: 'RD',
+      page: '03R&D_02생명정보학연구소'
+    },
+    {
+      stateClass: 'complete',
+      url: '03R&D_03더바이옴연구소',
+      category: 'RD',
+      page: '03R&D_03더바이옴연구소'
+    },
+    {
+      stateClass: 'complete',
+      url: '03R&D_04싱크진연구소',
+      category: 'RD',
+      page: '03R&D_04싱크진연구소'
+    },
+    {
+      stateClass: 'complete',
+      url: '03R&D_05빅데이터연구소',
+      category: 'RD',
+      page: '03R&D_05빅데이터연구소'
+    },
+    {
+      stateClass: 'complete',
+      url: '06특허,논문_01특허_01목록',
+      category: 'RD',
+      page: '06특허,논문_01특허_01목록'
+    },
+    {
+      stateClass: 'complete',
+      url: '06특허,논문_02논문_01목록',
+      category: 'RD',
+      page: '06특허,논문_02논문_01목록'
+    },
+    {
+      stateClass: 'complete',
+      url: '06특허,논문_02논문_01목록없음',
+      category: 'RD',
+      page: '06특허,논문_02논문_01목록없음'
+    },
+  ]
+}
+data.newsroom = {
+  title: 'NEWSROOM',
+  list: [
+    {
+      stateClass: 'complete',
+      url: '01뉴스_01목록',
+      category: 'newsroom',
+      page: '01뉴스_01목록'
+    },
+    {
+      stateClass: 'complete',
+      url: '01뉴스_01목록없음',
+      category: 'newsroom',
+      page: '01뉴스_01목록없음'
+    },
+    {
+      stateClass: 'complete',
+      url: '01뉴스_02조회',
+      category: 'newsroom',
+      page: '01뉴스_02조회'
+    },
+    {
+      stateClass: 'complete',
+      url: '02미디어라이브러리_01목록',
+      category: 'newsroom',
+      page: '02미디어라이브러리_01목록'
+    },
+    {
+      stateClass: 'complete',
+      url: '02미디어라이브러리_02조회',
+      category: 'newsroom',
+      page: '02미디어라이브러리_02조회'
+    },
+    {
+      stateClass: 'complete',
+      url: '02미디어라이브러리_02조회팝업',
+      category: 'newsroom',
+      page: '02미디어라이브러리_02조회팝업'
+    },
+    {
+      stateClass: 'complete',
+      url: '03공지사항_01목록',
+      category: 'newsroom',
+      page: '03공지사항_01목록'
+    },
+    {
+      stateClass: 'complete',
+      url: '03공지사항_02조회',
+      category: 'newsroom',
+      page: '03공지사항_02조회'
+    }
+  ]
+}
+data.IR = {
+  title: 'IR',
+  list: [
+    {
+      stateClass: 'complete',
+      url: '01재무정보',
+      category: 'IR',
+      page: '01재무정보'
+    },
+    {
+      stateClass: 'complete',
+      url: '02공시정보_01공고_01목록',
+      category: 'IR',
+      page: '02공시정보_01공고_01목록'
+    },
+    {
+      stateClass: 'complete',
+      url: '02공시정보_01공고_02조회',
+      category: 'IR',
+      page: '02공시정보_01공고_02조회'
+    },
+    {
+      stateClass: 'complete',
+      url: '02공시정보_02공시정보',
+      category: 'IR',
+      page: '02공시정보_02공시정보'
+    },
+    {
+      stateClass: 'complete',
+      url: '03IR소식_01목록',
+      category: 'IR',
+      page: '03IR소식_01목록'
+    },
+    {
+      stateClass: 'complete',
+      url: '03IR소식_02조회',
+      category: 'IR',
+      page: '03IR소식_02조회'
+    },
+    {
+      stateClass: 'complete',
+      url: '04투자자문의',
+      category: 'IR',
+      page: '04투자자문의'
+    }
+  ]
+}
+data.etc = {
+  title: 'Etc',
+  list: [
+    {
+      stateClass: 'complete',
+      url: '01개인정보처리방침',
+      category: 'etc',
+      page: '01개인정보처리방침'
+    },
+    {
+      stateClass: 'complete',
+      url: '02영상정보_운영_관리',
+      category: 'etc',
+      page: '02영상정보_운영_관리'
+    },
+    {
+      stateClass: 'complete',
+      url: '03내부정보관리규정',
+      category: 'etc',
+      page: '03내부정보관리규정'
+    },
+    {
+      stateClass: 'complete',
+      url: '에러페이지',
+      category: 'etc',
+      page: '에러페이지'
+    },
+    {
+      stateClass: 'complete',
+      url: '팝업페이지',
+      category: 'etc',
+      page: '팝업페이지'
+    },
+  ]
+}
+data.esg = {
+  title: 'ESG',
+  list: [
+    {
+      stateClass: 'complete',
+      url: '06ESG_01ESG경영',
+      category: 'ESG',
+      page: '06ESG_01ESG경영'
+    },
+    {
+      stateClass: 'complete',
+      url: '06ESG_02윤리경영',
+      category: 'ESG',
+      page: '06ESG_02윤리경영'
+    },
+    {
+      stateClass: 'complete',
+      url: '06ESG_03사회공헌_01소개',
+      category: 'ESG',
+      page: '06ESG_03사회공헌_01소개'
+    },
+    {
+      stateClass: 'complete',
+      url: '06ESG_03사회공헌_02과학자상목록',
+      category: 'ESG',
+      page: '06ESG_03사회공헌_02과학자상목록'
+    },
+    {
+      stateClass: 'complete',
+      url: '06ESG_03사회공헌_03과학자상상세',
+      category: 'ESG',
+      page: '06ESG_03사회공헌_03과학자상상세'
+    },
+  ]
+}
