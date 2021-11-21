ce87c9fe489b4580029facb9214c24d4079739e0
taeyoon <Gotaeyoon007@gmail.com>
2021-11-21 13:22:16 +0900

    동물 모델 사업부 완료

--- mobile-ko/pagelist/data/data.js
+++ mobile-ko/pagelist/data/data.js
@@ -335,7 +335,7 @@ data.business ={
         //     page: '연구서비스_MICROARRAY'
         // },
         {
-            stateClass: 'working',
+            stateClass: 'complete',
             url: '연구서비스_동물모델사업부',
             category: 'business',
             page: '연구서비스_동물모델사업부'
@@ -358,12 +358,12 @@ data.business ={
             category: 'business',
             page: '임상진단서비스_covid19'
         },
-        // {
-        //     stateClass: 'complete',
-        //     url: '헬스케어서비스_개인유전자분석',
-        //     category: 'business',
-        //     page: '헬스케어서비스_개인유전자분석'
-        // },
+        {
+            stateClass: 'working',
+            url: '헬스케어서비스_개인유전자분석',
+            category: 'business',
+            page: '헬스케어서비스_개인유전자분석'
+        },
         {
             stateClass: 'complete',
             url: '헬스케어서비스_장내미생물분석',
